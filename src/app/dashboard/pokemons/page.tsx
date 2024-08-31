f71595603f222c451ea04216530a7ce1920f4a51
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-08-31 17:25:28 +0200

    error page added

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -11,6 +11,7 @@ const getPokemons = async(limit= 20, offset=0): Promise<SimplePokemon[]>=>{
         id: pokemon.url.split('/').at(-2)!,
         name: pokemon.name,
     }))
+    throw new Error('This is a error');
 
 
     return pokemons;
