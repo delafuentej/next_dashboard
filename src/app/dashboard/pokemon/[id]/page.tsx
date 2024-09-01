683e5ba112602164a6dec3e7739ce22a5954b324
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-01 11:40:10 +0200

    obtain metadata dynamically: generateMetadata()

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import { PokemonsResponse } from '@/pokemons';
+import { Metadata } from 'next';
 interface Props {
     params: {id: string}
 }
 
-export const metadata = {
-    title:'Pokemon Page',
-    description: 'Pokemon Page Info',
-}
+// export const metadata = {
+//     title:'Pokemon Page',
+//     description: 'Pokemon Page Info',
+// }
 
+export const generateMetadata = async({params}: Props): Promise<Metadata>=>{
+    const {id, name} = await getPokemon(params.id);
+    return{
+        title:`${id}-${name}`,
+        description: `Pokemon Page Info: ${name} `,
+    }
+}
 const getPokemon = async(id: string): Promise<PokemonsResponse>=>{
     const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`,{
         cache:'force-cache'
+       // next: {
+          //  revalidate: 60 * 60 * 30 * 6
+       // }
     })
         .then( res => res.json());
 
@@ -27,7 +38,7 @@ const Pokemon = async({params}: Props) => {
   return (
     <>
         <div>Pokemon {params.id}</div>
-        <div>{JSON.stringify(pokemon)}</div>
+        <div>{JSON.stringify(pokemon.name)}</div>
     </>
     
   )
