9adf03af3acadad56f521f672503f733507cab0e
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-07 12:59:17 +0200

    widgetsGrid component

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,6 +1,7 @@
 import { SimpleWidget } from '@/components';
 import React from 'react';
 import { Metadata } from 'next';
+import WidgetsGrid from '@/components/dashboard/WidgetsGrid';
 
 export const metadata: Metadata = {
   title: 'Admin Dashboard',
@@ -13,21 +14,7 @@ const Main = () => {
       <h1 className='mt-2 text-3xl'>Dashboard</h1>
       <span className='text-xl'>General Info</span>
 
-      <div className='flex flex-wrap items-center justify-center p-2'>
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
-      </div>
+     <WidgetsGrid />
     </div>
   )
   
---
+++ src/components/dashboard/WidgetsGrid.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React from 'react'
+import { SimpleWidget } from './SimpleWidget';
+import { IoCartOutline } from 'react-icons/io5';
+import { useAppSelector } from '../../store/index';
+
+const WidgetsGrid = () => {
+    const isCart = useAppSelector( state => state.counter.count)
+  return (
+        <div className='flex flex-wrap items-center justify-center p-2'>
+        <SimpleWidget 
+            title={`${isCart}`}
+            subTitle='Products added'
+            label='Counter'
+            icon = {<IoCartOutline size={70} className='text-blue-600'/>}
+            />
+        {/* <SimpleWidget  title=''/> */}
+      
+      </div>
+  )
+}
+
+export default WidgetsGrid
